c2421686d45c2a819db321be0ac0b8c6f60374d6
Takayuki Sei <takayuki_sei@voyagegroup.com>
2013-11-16 23:52:51 +0900

    added 部屋オブジェクトとカメラ回転機能 #1

---
+++ Assets/MainCameraScript.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function Update () {
+	var axis = Input.GetAxisRaw("Horizontal");
+	
+	if (axis == 1) {
+		transform.eulerAngles.y += 90.0;
+	} else if (axis == -1) {
+		transform.eulerAngles.y -= 90.0;
+	}
+}
